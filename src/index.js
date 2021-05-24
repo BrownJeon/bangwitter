804d5f4d52ca911a31a6ab35b2a1fc1b2e7f0d22
전병훈 <ifries8611@gmail.com>
2021-05-24 13:21:13 +0900

    기본 router설정

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
 import firebase from "./firebase";
 
-console.log(firebase);
-
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App/>,
+    document.getElementById('root')
 );
 
