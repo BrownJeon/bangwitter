0ca827b8921ae5991432c545d7719576cbc37b32
전병훈 <ifries8611@gmail.com>
2021-05-26 22:31:09 +0900

    tweet를 위한 from정의

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+    const [bangWeet, setBangWeet] = useState("");
+    const onSubmit = (event) => {
+        event.preventDefault();
+    };
+    const onChange = (event) => {
+        const {
+            target: { value },
+        } = event;
+        setBangWeet(value);
+    };
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input
+                    value={bangWeet}
+                    onChange={onChange}
+                    type="text"
+                    placeholder="What's on your mind?"
+                    maxLength={120}
+                />
+                <input type="submit" value="bangWeet" />
+            </form>
+        </div>
+    );
+};
+
 export default Home;
