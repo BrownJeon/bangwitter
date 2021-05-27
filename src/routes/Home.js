ad40764f0c7b5db6ea2c33d417095f8bc0100711
전병훈 <ifries8611@gmail.com>
2021-05-27 13:15:49 +0900

    등록한 tweet을 목록에 render해주는 기능 추가

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,16 +1,39 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
+import {dbService} from "../fbase";
 
 const Home = () => {
     const [bangWeet, setBangWeet] = useState("");
-    const onSubmit = (event) => {
+    const [bangWeets, setBangWeets] = useState([]);
+    const getBangWeets = async () => {
+        const dbBangWeet = await dbService.collection("bangWeets").get();
+        dbBangWeet.forEach(document => {
+            const bangWeetObj = {
+                ...document.data(),
+                id: document.id,
+            };
+            setBangWeets((prev) => [...prev, bangWeetObj]);
+        })
+    };
+
+    useEffect(() => {
+        getBangWeets();
+    }, []);
+
+    const onSubmit = async (event) => {
         event.preventDefault();
+        await dbService.collection("bangWeets").add({
+            text: bangWeet,
+            createAt: Date.now()
+        });
+        setBangWeet("");
     };
     const onChange = (event) => {
         const {
-            target: { value },
+            target: {value},
         } = event;
         setBangWeet(value);
     };
+    // console.log(bangWeets);
     return (
         <div>
             <form onSubmit={onSubmit}>
@@ -21,10 +44,18 @@ const Home = () => {
                     placeholder="What's on your mind?"
                     maxLength={120}
                 />
-                <input type="submit" value="bangWeet" />
+                <input type="submit" value="BangWeet"/>
             </form>
+            <div>
+                {bangWeets.map((bangWeet) => (
+                    <div key={bangWeet.id}>
+                        <h4>{bangWeet.bangWeet}</h4>
+                    </div>
+                ))}
+            </div>
         </div>
-    );
+    )
+        ;
 };
 
 export default Home;
